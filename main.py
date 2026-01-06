0b7d6fd18619caef93abdfc9063168249425bb8b
XNeedNoName <xneednoname@tutamail.com>
2026-01-06 11:07:11 +0100

    Add async notifier for downloading scrim data

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+import datetime
+import time
+import requests
+
+def download(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.content
+    return None
+
+from desktop_notifier import DesktopNotifier
+basestring = "https://hiddengems.gymnasiumsteglitz.de/dl/stats/"
+timestring = datetime.datetime.now().strftime("%Y-%m-%d")
+notifier = DesktopNotifier()
+
+async def main():
+    await notifier.send(title="Scrims Released!", message="Hidden Gems Update")
+while True:
+    if download(basestring + timestring + ".json.gz") is not None:
+        asyncio.run(main())
+    else:
+        print("No Scrim Data yet")
+    time.sleep(1)
